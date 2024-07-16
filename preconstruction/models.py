b602289ce151cc5173d184d77c3152d6ca412311
vishaldhakal <vishaldhakal96@gmail.com>
2024-07-16 20:24:51 +0545

    dfv'

--- preconstruction/models.py
+++ preconstruction/models.py
@@ -76,7 +76,7 @@ class PreConstruction(models.Model):
         return self.project_name + " [ " + self.city.name+" ] "
 
     class Meta:
-        ordering = ('-last_updated',)
+        ordering = ('-status','-last_updated',)
 
 
 class PreConstructionImage(models.Model):
